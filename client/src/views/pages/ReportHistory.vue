1b54ac0a3f62cd33aff9e4bcba271ec362ccc862
Joel Dsouza <joeld@mkcl.org>
2018-12-15 00:31:36 +0530

    Report History
    Report History

--- client/src/views/pages/ReportHistory.vue
+++ client/src/views/pages/ReportHistory.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="wrapper">
     <div class="animated fadeIn">
-      <b-row>
-        <b-col cols="12"></b-col>
+      <!-- <b-row>
+        <b-col >hi</b-col>
+        <b-col >hi</b-col>
+        <b-col >hi</b-col>
+        <b-col >hi</b-col>
       </b-row>
+      <b-row>
+        
+      </b-row> -->
+      <b-container class="bv-example-row" fluid>
+        <!-- <b-row > 
+            <b-col cols="3"><h4>Sr. No</h4></b-col>
+            <b-col> <h4> Name of Report </h4></b-col>
+            <b-col> <h4> Analytic Dashboard </h4></b-col>
+        </b-row> -->
+        <!-- <b-row v-for="(singleEntry,index) in usersHistory" >
+            <b-col cols="3">{{index+1}}</b-col>
+            <b-col>{{singleEntry.reportName}}</b-col>
+            <b-col>
+              <div v-for="singleDashboard in singleEntry.analyticDashboard">
+                {{singleDashboard}}
+              </div>
+            </b-col>
+        </b-row> -->
+        <b-row>
+          <b-table hover :items="usersHistory" >
+            <template slot="analyticDashboard" slot-scope="data">
+              <b-row v-for="singleEntry in data.value" > 
+                <b-col><a href="#">{{singleEntry}}</a></b-col>
+              </b-row>
+            </template>
+          </b-table>
+        </b-row>
+    </b-container>
     </div>
   </div>
 </template>
@@ -16,7 +47,9 @@ import axios from "axios";
 export default {
   name: "ReportHistory",
   data() {
-    return {};
+    return {
+      usersHistory : [{"reportName":"Exam Report","analyticDashboard":["Sex Ratio","No of Passing Students","Leaked Paper","Failed Marks"]},{"reportName":"Survey","analyticDashboard":["Food Samples Sold","Food Samples Spoilt"]},{"reportName":"Survey","analyticDashboard":[]},{"reportName":"Survey"}]
+    };
   },
   methods: {},
   mounted() {

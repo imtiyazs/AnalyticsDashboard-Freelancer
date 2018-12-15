56277a686ed22161fdcdfc8945827eb8ddcae7fe
Joel Dsouza <joeld@mkcl.org>
2018-12-15 20:57:57 +0530

    Completed_ReportAnalysis
    Completed_ReportAnalysis (CSV,SAV)

--- client/src/views/pages/ReportHistory.vue
+++ client/src/views/pages/ReportHistory.vue
@@ -1,34 +1,14 @@
 <template>
   <div class="wrapper">
     <div class="animated fadeIn">
-      <!-- <b-row>
-        <b-col >hi</b-col>
-        <b-col >hi</b-col>
-        <b-col >hi</b-col>
-        <b-col >hi</b-col>
-      </b-row>
-      <b-row>
-        
-      </b-row> -->
       <b-container class="bv-example-row" fluid>
-        <!-- <b-row > 
-            <b-col cols="3"><h4>Sr. No</h4></b-col>
-            <b-col> <h4> Name of Report </h4></b-col>
-            <b-col> <h4> Analytic Dashboard </h4></b-col>
-        </b-row> -->
-        <!-- <b-row v-for="(singleEntry,index) in usersHistory" >
-            <b-col cols="3">{{index+1}}</b-col>
-            <b-col>{{singleEntry.reportName}}</b-col>
-            <b-col>
-              <div v-for="singleDashboard in singleEntry.analyticDashboard">
-                {{singleDashboard}}
-              </div>
-            </b-col>
-        </b-row> -->
         <b-row>
           <b-table hover :items="usersHistory" >
+            <template slot="reportName" slot-scope="data">
+              <a href="#">{{data.value}}</a>
+            </template>
             <template slot="analyticDashboard" slot-scope="data">
-              <b-row v-for="singleEntry in data.value" > 
+              <b-row v-for="(singleEntry,index) in data.value" :key="index"> 
                 <b-col><a href="#">{{singleEntry}}</a></b-col>
               </b-row>
             </template>
@@ -48,7 +28,7 @@ export default {
   name: "ReportHistory",
   data() {
     return {
-      usersHistory : [{"reportName":"Exam Report","analyticDashboard":["Sex Ratio","No of Passing Students","Leaked Paper","Failed Marks"]},{"reportName":"Survey","analyticDashboard":["Food Samples Sold","Food Samples Spoilt"]},{"reportName":"Survey","analyticDashboard":[]},{"reportName":"Survey"}]
+      usersHistory : [{"reportName":"Exam Report","analyticDashboard":["Sex Ratio","No of Passing Students","Leaked Paper","Failed Marks"]},{"reportName":"Survey","analyticDashboard":["Food Samples Sold","Food Samples Spoilt"]},{"reportName":"Survey 500","analyticDashboard":[]},{"reportName":"Survey100"}]
     };
   },
   methods: {},

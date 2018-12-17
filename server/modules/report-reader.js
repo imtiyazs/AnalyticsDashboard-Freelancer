c7ef7cfc9c2d6f2545d71cff4a7aeca60dc180d6
Imtiyaz Shaikh <imtiyazs@mkcl.org>
2018-12-17 22:16:29 +0530

    Get specific reports history

--- server/modules/report-reader.js
+++ server/modules/report-reader.js
@@ -4,15 +4,44 @@ const database = require('../database/database'),
 
 exports.FetchUserReports = (requestBody) => {
     return new Promise((resolve, reject) => {
-        let username = requestBody.username
-        database.FindOneInCollection({
-            username: username
-        }, constants.ReportsCollection, (data) => {
-            if (data == null || data.length == 0) {
-                return reject(false)
-            }
+        let username = requestBody.username,
+            limit = requestBody.limit
 
-            resolve(data)
-        })
+        if (limit == undefined || limit == 0 || limit == null) {
+            database.FindOneInCollection({
+                username: username
+            }, constants.ReportsCollection, (data) => {
+                if (data == null || data.length == 0) {
+                    return reject(false)
+                }
+
+                resolve(data)
+            })
+        } else {
+
+            database.FindOneInCollection({
+                username: username
+            }, constants.ReportsCollection, (data) => {
+                if (data == null || data.length == 0) {
+                    return reject(false)
+                }
+
+                try {
+
+                    let dataArr = data.reports.slice(Math.max(data.reports.length - limit, 1))
+
+                    let dataObj = {
+                        username: data.username,
+                        reports: dataArr.reverse()
+                    }
+
+                    resolve(dataObj)
+                } catch (err) {
+                    logger.error('Error fetching last elements from DB: ' + err)
+                    return reject(false)
+                }
+            })
+
+        }
     })
 }

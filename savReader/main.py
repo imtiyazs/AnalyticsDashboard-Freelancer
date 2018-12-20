2b1c4d16857072d617e9b1317f7d60b936332c97
Imtiyaz Shaikh <imtiyazs@mkcl.org>
2018-12-21 00:53:08 +0530

    Remove blank data

--- savReader/main.py
+++ savReader/main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import savReaderWriter as s
 import codecs
 import sys
 import json
 import re
 import os
+import savReaderWriter as s
 
 # Check for commandline args
 if len(sys.argv) != 3:
@@ -19,13 +19,36 @@ _, fileExtension = os.path.splitext(sourceFile)
 if fileExtension == '.xlsx' or fileExtension == '.xls':
     data = {}
     # Load spreadsheet
-    xl = pd.read_excel(sourceFile)
+    df = pd.read_excel(sourceFile)
     # For each in headers
-    for i, each in enumerate(xl.columns):
+    #for i, each in enumerate(xl.columns):
          # Decode header to text and set as JSON key. Select first column of dataframe and convert to NPArray
-        data[each.decode("utf-8")] = xl.iloc[:, i].values.astype(str).tolist()
+    #    data[each.decode("utf-8")] = xl.iloc[:, i].values.astype(str).tolist()
+
+    df.dropna(axis=0, inplace=True)
+
+    #drop duplicate values
+    df.drop_duplicates(keep='last',inplace=True)
+
+    #fetch data type of columns
+    dtypeList = df.dtypes
+    i=0
+    for singleColumn in df.columns:
+        #Sent all column data
+        #data[singleColumn] = df[singleColumn].values.tolist()
+        
+        #Sent int & float columns only
+        if dtypeList[i]!='object' :
+            data[singleColumn] = df[singleColumn].values.tolist()
+        i = i+1
+
+
+
+
+
+
     # Save to destination
-        json.dump(data, codecs.open(destinationFile, 'w',
+    json.dump(data, codecs.open(destinationFile, 'w',
                                     encoding='utf-8'), sort_keys=True, indent=4)
 
 #  READ SAV OR SPSS FILES
@@ -40,10 +63,54 @@ if fileExtension == '.sav':
     data = {}
 
     # For each in headers
-    for i, each in enumerate(headerInBytes):
+    #for i, each in enumerate(headerInBytes):
         # Decode header to text and set as JSON key. Select first column of dataframe and convert to NPArray
-        data[each.decode("utf-8")] = df.iloc[:, i].values.astype(str).tolist()
+    #    data[each.decode("utf-8")] = df.iloc[:, i].values.astype(str).tolist()
+
+    #byte to string
+    dfColumns = [singleColumn.decode("utf-8") for singleColumn in headerInBytes]
+
+    #Set names of columns
+    df.columns = dfColumns
+
+    dtypeList = df.dtypes
+    i=0
+    for singleColumn in df.columns:
+        #Sent all column data
+        #data[singleColumn] = df[singleColumn].values.tolist()
+        
+        #Sent int & float columns only
+        if dtypeList[i]!='object' :
+            data[singleColumn] = df[singleColumn].values.tolist()
+        i = i+1
 
     # Save to destination
     json.dump(data, codecs.open(destinationFile, 'w',
                                 encoding='utf-8'), sort_keys=True, indent=4)
+
+if fileExtension == '.csv':
+    data = {}
+    # Load spreadsheet
+    df = pd.read_csv(sourceFile)
+
+    #drop missing values
+    df.dropna(axis=0, inplace=True)
+
+    #drop duplicate values
+    df.drop_duplicates(keep='last',inplace=True)
+
+    #fetch data type of columns
+    dtypeList = df.dtypes
+    i=0
+    for singleColumn in df.columns:
+        #Sent all column data
+        #data[singleColumn] = df[singleColumn].values.tolist()
+        
+        #Sent int & float columns only
+        if dtypeList[i]!='object' :
+            data[singleColumn] = df[singleColumn].values.tolist()
+        i = i+1
+
+    # Save to destination
+    json.dump(data, codecs.open(destinationFile, 'w',
+                                    encoding='utf-8'), sort_keys=True, indent=4)
